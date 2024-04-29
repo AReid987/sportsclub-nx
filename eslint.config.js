46e0af00b9e7aa284bcfee6730e0b20752429a96
Antonio Reid <read.musik@gmail.com>
2024-04-29 00:47:23 -0500

    nx format:write

--- eslint.config.js
+++ eslint.config.js
@@ -1,58 +1,51 @@
+// @ts-check
 const { FlatCompat } = require('@eslint/eslintrc');
 const nxEslintPlugin = require('@nx/eslint-plugin');
 const js = require('@eslint/js');
-const globals = require('globals')
+const globals = require('globals');
 const jsoncParser = require('jsonc-eslint-parser');
-const tsParser = require('@typescript-eslint/parser');
+// const tsParser = require('@typescript-eslint/parser');
+const tsEslint = require('typescript-eslint');
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 module.exports = [
-    { plugins: { '@nx': nxEslintPlugin } },
-    {
-        rules: {
-            '@typescript-eslint/explicit-module-boundary-types': ['error'],
-        },
-        languageOptions: {
-            parser: tsParser,
-            globals: {
-                ...globals.node,
+  { plugins: { '@nx': nxEslintPlugin } },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
             },
+          ],
         },
+      ],
     },
-    {
-        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-        rules: {
-            '@nx/enforce-module-boundaries': [
-                'error',
-                {
-                    enforceBuildableLibDependency: true,
-                    allow: [],
-                    depConstraints: [{
-                        sourceTag: '*',
-                        onlyDependOnLibsWithTags: ['*'],
-                    }, ],
-                },
-            ],
-        },
+  },
+  {
+    files: ['*.json'],
+    languageOptions: {
+      parser: jsoncParser,
     },
-    {
-        files: ['*.json'],
-        languageOptions: {
-            parser: jsoncParser,
-        },
-        rules: {},
-    },
-    ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
-        ...config,
-        files: ['**/*.ts', '**/*.tsx'],
-        rules: {},
-    })),
-    ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
-        ...config,
-        files: ['**/*.js', '**/*.jsx'],
-        rules: {},
-    })),
+    rules: {},
+  },
+  ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  })),
+  ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  })),
 ];
