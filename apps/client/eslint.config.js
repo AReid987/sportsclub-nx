71545bc0a57d2122a355c95390c969d16318bdb9
Antonio Reid <read.musik@gmail.com>
2024-05-05 20:42:46 -0500

    nx targets working except for client:lint pt 2

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -3,34 +3,34 @@ const baseConfig = require('../../eslint.config.js');
 const js = require('@eslint/js');
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
 });
 
 module.exports = [
-  ...baseConfig,
-  ...compat.extends(
-    'plugin:@nx/react-typescript',
-    'next',
-    'next/core-web-vitals',
-  ),
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@next/next/no-html-link-for-pages': ['error', 'apps/client/pages'],
+    ...baseConfig,
+    ...compat.extends(
+        'plugin:@nx/react-typescript',
+        'next',
+        'next/core-web-vitals',
+    ),
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+        rules: {
+            '@next/next/no-html-link-for-pages': ['error', 'apps/client/pages'],
+        },
     },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {},
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    rules: {},
-  },
-  ...compat.config({ env: { jest: true } }).map((config) => ({
-    ...config,
-    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
-  })),
-  { ignores: ['.next/**/*'] },
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {},
+    },
+    {
+        files: ['**/*.js', '**/*.jsx'],
+        rules: {},
+    },
+    ...compat.config({ env: { jest: true } }).map((config) => ({
+        ...config,
+        files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+    })),
+    { ignores: ['.next/**/*'] },
 ];
